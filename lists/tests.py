f176dcb129ec55b4b2bdf4b4c56ac31e714238a0
Noah V <n_villela@yahoo.com>
2017-12-30 03:45:25 +0000

    Generates lists to only-list-in-world url

--- lists/tests.py
+++ lists/tests.py
@@ -28,6 +28,10 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 class ListViewTest(TestCase):
     
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+    
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
