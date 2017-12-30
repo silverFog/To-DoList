7a8d3efaacaf3e530ea3dca4e46fca177b8c2ee0
Noah V <n_villela@yahoo.com>
2017-12-30 03:43:00 +0000

    Generates lists to only-list-in-world url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,8 +40,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+            'Enter a to-do item')
 
         # she types "Buy peacock feather" into a text box (Edith's hobby
         # is tying fly-fishing lures)
@@ -55,6 +54,7 @@ class NewVisitorTest(LiveServerTestCase):
         # There is still a text box inviting her to add another item. She
         # enters "use peacock feathers to make a fly" (Edith is very
         # methodical)
+        #time.sleep(0.5)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
