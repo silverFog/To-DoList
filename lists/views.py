b89282e1d10355edc2af05ed83b058c76abb60bc
Noah V <n_villela@yahoo.com>
2017-12-30 06:09:54 +0000

    broke something, fixed others.

--- lists/views.py
+++ lists/views.py
@@ -8,9 +8,6 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 def home_page(request):
-    if request.method == 'POST':        
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 def new_list(request):
     return redirect('/lists/the-only-list-in-the-world/')
