58fa0fae4407a7fb42762b55ab5c06e7c60ea683
Noah V <n_villela@yahoo.com>
2018-01-08 01:47:27 +0000

    use more env vars for DEBUG and SECRET_KEY

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,11 +19,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#)$6=sc9!4@o#3^0ukqv591oy)az#%m200*pqrwz8gu%%j_@ws'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
 
 ALLOWED_HOSTS = [
     '159.89.176.151',
